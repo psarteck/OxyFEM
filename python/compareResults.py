02d930928d9d128fb569cbd6a2e221865ae2ad74
Matthieu <matthieu.petit24@gmail.com>
2024-09-18 13:49:32 +0200

    Small modifications in the python codes

--- python/compareResults.py
+++ python/compareResults.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 ## FreeFem Code
 ######################################################################
-commandFree = ["FreeFem++", "/Users/djo/Desktop/MEF/C++/FREEFEMTESTS/pb3.edp"]  
+commandFree = ["FreeFem++", "/Users/djo/Dev/OxyFEM/FREEFEMTESTS/pb3.edp"]  
 
 resultatFree = subprocess.run(commandFree, capture_output=True, text=True)
 
@@ -22,7 +22,7 @@ print(resultatFree.stdout)
 # print(resultatOxy.stdout)
 # ######################################################################
 
-doss = "/Users/djo/Desktop/MEF/C++/results/"
+doss = "/Users/djo/Dev/OxyFEM/results/"
 
 
 fileFree = "solPb3.txt"
@@ -35,7 +35,7 @@ solOxy = np.loadtxt(doss + fileOxy)
 error = np.abs(solOxy - solFree[:,2])
 
 relativeError = np.linalg.norm(error) / np.linalg.norm(solFree[:,2])
-print("Relative error : ", np.round(relativeError, 4), "%")
+print("Relative error : ", np.round(relativeError, 4)*100, "%")
 
 size = len(solFree[:,0])
 
@@ -56,27 +56,30 @@ plt.legend()
 
 ## 2D comparaison
 ######################################################################
-x = solFree[:, 0].astype(int)
-y = solFree[:, 1].astype(int)
-resultFree = solFree[:, 2]
-resultFree = solOxy
+# x = solFree[:, 0].astype(int)
+# y = solFree[:, 1].astype(int)
+# resultFree = solFree[:, 2]
+# resultFree = solOxy
 
 
-taille = np.sqrt(len(resultFree[:])).astype(int)
-U = resultFree.reshape((taille,taille))
-x = np.linspace(0, 3, taille)
-y = np.linspace(0, 3, taille)
-X, Y = np.meshgrid(x, y)
+# taille = np.sqrt(len(resultFree[:])).astype(int)
+# U = resultFree.reshape((taille,taille))
+# x = np.linspace(0, 3, taille)
+# y = np.linspace(0, 3, taille)
+# X, Y = np.meshgrid(x, y)
 
 
-fig = plt.figure(figsize=(10,8))
-ax = fig.add_subplot(111, projection='3d')
-ax.grid(False)
-surf = ax.plot_surface(X,Y,U, cmap='turbo', shade=True, rstride=1, cstride=1, alpha=None, antialiased=True)
-fig.colorbar(surf)
-ax.view_init(45, 0)
-plt.xlabel('X', fontsize=22)
-plt.ylabel('Y', fontsize=22)
+# fig = plt.figure(figsize=(10,8))
+# ax = fig.add_subplot(111, projection='3d')
+# ax.grid(False)
+# surf = ax.plot_surface(X,Y,U, cmap='turbo', shade=True, rstride=1, cstride=1, alpha=None, antialiased=True)
+# fig.colorbar(surf)
+# ax.view_init(45, 0)
+# plt.xlabel('X', fontsize=22)
+# plt.ylabel('Y', fontsize=22)
+
+
+## To Delete ?
 
 # x_max, y_max = x.max() + 1, y.max() + 1
 
--- python/oxySol3D.py
+++ python/oxySol3D.py
@@ -9,22 +9,25 @@ data = np.loadtxt(name)
 tab = data[:].astype(float)
 taille = np.sqrt(len(tab[:])).astype(int)
 
-print(taille)
+# print(taille)
 U = tab.reshape((taille,taille))
 x = np.linspace(0, 1, taille)
 y = np.linspace(0, 1, taille)
 X, Y = np.meshgrid(x, y)
 
-print(U)
+# print(U)
 
-uex = np.cos(np.pi * X) * np.cos(np.pi * Y)
-# uex = np.sin(np.pi * X) * np.sin(np.pi * Y)
+# uex = np.cos(np.pi * X) * np.cos(np.pi * Y)
+uex = np.sin(np.pi * X) * np.sin(np.pi * Y)
 errorAbs = np.linalg.norm(U - uex)
 absUex = np.linalg.norm(uex)
 relativeError = errorAbs / absUex
 txtRelErr = np.round(relativeError, 5) * 100
 
+print()
+print("Python code for the comparaison data...")
 print("Relative Error : ", txtRelErr)
+print()
 
 fig = plt.figure(figsize=(10,8))
 ax = fig.add_subplot(111, projection='3d')
